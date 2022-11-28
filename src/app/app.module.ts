cc14570ac641a51a596a897535f969ce5cfea55f
Gustavo Hernandez <gustavo.hernandez@kint.com.mx>
2022-11-28 16:39:40 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AreaComponent } from './area/area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    AreaComponent
   ],
   imports: [
     BrowserModule,
